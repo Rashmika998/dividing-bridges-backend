d7e2d88f50f8c2464e780090d0e600a4665b6b0f
Rashmika998 <silvarashmika98@gmail.com>
2025-01-05 22:21:20 +0530

    initial commit

---
+++ api-gateway/src/main/java/org/penpal/api/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package org.penpal.api.gateway.filter;
+
+import io.jsonwebtoken.Claims;
+import org.penpal.api.gateway.exception.JwtTokenMalformedException;
+import org.penpal.api.gateway.exception.JwtTokenMissingException;
+import org.penpal.api.gateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class JwtAuthenticationFilter implements GatewayFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
+
+//        Bypass endpoint
+        final List<String> apiEndpoints = List.of(
+                "api/auth/login",
+                "/api/auth/students",
+                "/api/auth/teachers",
+                "/api/auth/translators",
+                "/api/auth/researchers");
+
+        Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
+                .noneMatch(uri -> r.getURI().getPath().contains(uri));
+
+        if (isApiSecured.test(request)) {
+            if (!request.getHeaders().containsKey("Authorization")) {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+                return response.setComplete();
+            }
+
+            final String token = request.getHeaders().getOrEmpty("Authorization").get(0);
+
+            try {
+                jwtUtil.validateToken(token);
+            } catch (JwtTokenMalformedException | JwtTokenMissingException e ) {
+
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.BAD_REQUEST);
+
+                return response.setComplete();
+            }
+
+            Claims claims = jwtUtil.getClaims(token);
+            exchange.getRequest().mutate().header("id", String.valueOf(claims.get("id"))).build();
+        }
+
+        return chain.filter(exchange);
+    }
+
+}
---
+++ auth/src/main/java/org/penpal/auth/security/JwtAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package org.penpal.auth.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.penpal.auth.validators.JwtValidator;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtValidator jwtValidator;
+
+    public JwtAuthenticationFilter( JwtValidator jwtValidator) {
+        this.jwtValidator = jwtValidator;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        final String token = authHeader.substring(7); // Remove "Bearer " prefix
+
+        if (!jwtValidator.validateToken(token)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String email = jwtValidator.extractEmail(token);
+        String userRole = jwtValidator.extractUserRole(token);
+
+        if (email != null) {
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(email, null, null); // Add roles if needed
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
